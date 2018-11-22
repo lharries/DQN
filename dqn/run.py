bb1ad361e1d7f7377989c3a8ed64d7f3e0756788
lharries <github@lukeharries.me>
2018-11-22 00:12:58 +0000

    simplified runner and added epsilon annealing

--- dqn/run.py
+++ dqn/run.py
@@ -1,27 +1,66 @@
-from dqn import EpsilonGreedyPolicy
 import gym
-import argparse
+import numpy as np
 
-from runner import Runner
+from replay_buffer import ReplayBuffer
+
+from dqn import EpsilonGreedyPolicy
 
 
-def main(episodes=5, env_name='CartPole-v1'):
-    env = gym.make(env_name)
-    policy = EpsilonGreedyPolicy(env.observation_space.shape, env.action_space.n)
+class Runner:
+    def __init__(self, env_name='CartPole-v1', epochs=5, nsteps=2e3, mini_batch_size=32, replay_buffer_size=100):
+        self.env = gym.make(env_name)
+        self.policy = EpsilonGreedyPolicy(self.env.observation_space.shape, self.env.action_space.n)
+        self.epochs = epochs
+        self.nsteps = nsteps
+        self.replay_buffer = ReplayBuffer(self.env.observation_space.shape, replay_buffer_size)
+        self.mini_batch_size = mini_batch_size
 
-    # import pdb; pdb.set_trace()
-    runner = Runner(env, policy)
+        # probability you choose a random action instead of following the policy
+        self.epsilon = 1.0
+        self.epsilon_final = 0.1
+        self.epsilon_anneal_steps = 1e3
+        self.epsilon_anneal_rate = (self.epsilon - self.epsilon_final) / self.epsilon_anneal_steps
 
+    def run(self):
+        print("training...\n")
+
+        step = 0
+
+        while True:
+
+            # run episode
+            observation = self.env.reset()
+            done = False
+
+            episode_reward = 0
+
+            while not done:
+                action = self.policy.compute_actions(observation)
+                new_observation, reward, done, info = self.env.step(action)
+
+                self.replay_buffer.add(observation, new_observation, reward, action)
+
+                # train
+                if step > 50:
+                    mini_batch = self.replay_buffer.sample(self.mini_batch_size)
+                    # TODO: Update models
+
+                # anneal the self.epsilon value
+                if self.epsilon > self.epsilon_final:
+                    self.epsilon -= self.epsilon_anneal_rate
+
+                episode_reward += reward
+                step += 1
+
+            print(f'Step number {step}, reward: {episode_reward}')
+
+            if step >= self.nsteps:
+                return
+
+
+def main():
+    runner = Runner()
     runner.run()
-
-    print("\n---done---")
 
 
 main()
-
-# import gym
-# env = gym.make('CartPole-v0')
-# env.reset()
-# for _ in range(1000):
-#     env.render()
-#     env.step(env.action_space.sample()) # take a random action
